c8abd497f8bca1888838f78f2a6e9c4d8da9ca42
Shivam <samsersingh3133s@gmail.com>
2023-09-06 19:10:08 +0530

    initial commit This is a countdown project in which we can set any future date and this website will count down till thie date and time

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+const enddate = "10 September 2023 03:58:20 PM";
+document.getElementById('end-date').innerHTML = enddate;
+const remainning_time = document.querySelectorAll('input');
+function clock() {
+    const edate = new Date(enddate);
+    const now = new Date();
+    const diff = (edate - now) / 1000;  
+    // console.log(enddate);
+    // console.log(edate);
+    // console.log(now);
+    // console.log(diff);
+
+    if (diff < 0) {
+        return;
+    }
+
+    //Convert to days
+    remainning_time[0].value = Math.floor(diff / 3600 / 24);
+
+    // convert to hours
+    remainning_time[1].value = Math.floor((diff / 3600) % 24);
+
+    // convert to remaining minutes
+    remainning_time[2].value = Math.floor((diff / 60) % (60));
+
+    // convert to remaining seconds
+    remainning_time[3].value = Math.floor(diff % (60));
+
+}
+// initial call
+clock();
+
+setInterval(() => {
+    clock();
+}, 1000);
+
